b5adb6ba5964dd061736784a0ad2a588c64ef35c
Vera Patricio <vera.patricio@univ-lyon1.fr>
2019-05-14 17:05:32 +0200

    fixing docs -- reproject dependency

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,6 +18,20 @@ import sys
 sys.path.insert(0, os.path.abspath('../..'))
 sys.setrecursionlimit(1500)
 
+
+# Try to fix reproject issue
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+MOCK_MODULES = ['reproject']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
+
+
 '''import astropy
 
 try:
